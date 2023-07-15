5b946691e0c41d9aa6d100acc7492338b94c0d43
ZeynepDuzen <zeynepduzen59@gmail.com>
2023-07-15 17:17:25 +0300

    Proje dosyası ekle.

---
+++ IntVariables/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntVariables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi;
+            Console.WriteLine("Lütfen bir sayi giriniz : ");
+            sayi = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine();
+            Console.Write("Girmis oldugunuz sayi : " + sayi);
+
+            Console.Read();
+        }
+    }
+}
